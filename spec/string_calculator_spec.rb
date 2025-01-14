19a34d16bb39c813ea4ad1d89301aa680a126509
Ganesh Navale <navaleganesh2011@gmail.com>
2025-01-15 00:23:41 +0530

    Added test case for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    context 'when the input is a single number' do
+      it 'returns 0 when the string contains 0' do
+        expect(StringCalculator.add('0')).to eq(0)
+      end
+
+      it 'returns 32 when the string contains 32' do
+        expect(StringCalculator.add('32')).to eq(32)
+      end
+
+      it 'returns 9999 when the string contains 9999' do
+        expect(StringCalculator.add('9999')).to eq(9999)
+      end
+    end
   end
 end
