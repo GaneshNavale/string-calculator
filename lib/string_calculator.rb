c4683f77f08352af52747c10657360734ec66650
Ganesh Navale <navaleganesh2011@gmail.com>
2025-01-15 15:19:52 +0530

    Code Refactoring

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,30 +5,27 @@ module StringCalculator
     return 0 if str.empty?
 
     digits = extract_digits(str)
+    validate_negatives(digits)
 
-    negatives = digits.select(&:negative?)
-    raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
-
-    digits.reduce(0) { |sum, digit| sum += (digit <= 1000 ? digit : 0) }
+    digits.reduce(0) { |sum, digit| sum + (digit <= 1000 ? digit : 0) }
   end
 
   def self.delimiter(str)
-    if str.start_with?('//')
-      match = str.match(%r{//(\[.*?\])+\n})
-      if match
-        match[0].scan(/\[(.*?)\]/).flatten
-      else
-        [str[2]]
-      end
-    else
-      [',']
-    end
+    return [','] unless str.start_with?('//')
+
+    match = str.match(%r{//(\[.*?\])+\n})
+    match ? match[0].scan(/\[(.*?)\]/).flatten : [str[2]]
   end
 
   def self.extract_digits(str)
     delims = delimiter(str)
     numbers_str = str.start_with?('//') ? str.split("\n", 2).last : str
-    regex = Regexp.union(delims.map { |delim| delim })
+    regex = Regexp.union(delims)
     numbers_str.split(/#{regex}|\n/).map(&:to_i)
+  end
+
+  def self.validate_negatives(digits)
+    negatives = digits.select(&:negative?)
+    raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
