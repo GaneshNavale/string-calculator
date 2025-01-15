619673894c143a78e19eb0342286905dc77bf80b
Ganesh Navale <navaleganesh2011@gmail.com>
2025-01-15 11:41:35 +0530

    Implemented custom delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,12 @@ module StringCalculator
   def self.add(str)
     return 0 if str.empty?
 
-    digits = str.split(/\n|,/).map(&:to_i)
+    if str.start_with?('//')
+      delimiter = str[2]
+      digits = str.split(/\n|#{delimiter}/).map(&:to_i)
+    else
+      digits = str.split(/\n|,/).map(&:to_i)
+    end
     digits.reduce { |sum, x| sum + x }
   end
 end
